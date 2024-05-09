a2960a84508bc3779ae287717f6874f695383548
Guilherme Luan <guilhermeluan@pm.me>
2024-05-09 20:46:11 -0300

    feat: cadastro do client

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,57 @@
 package application;
 
+import entities.Client;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import static utils.ReadFile.readFile;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         ArrayList<String> listQuestions = new ArrayList<>();
+        ArrayList<Client> clients = new ArrayList<>();
 
         readFile("src/formulario.txt", listQuestions);
 
-        for (String i: listQuestions) {
-            System.out.println(i);
+        String name = null, email = null;
+        int age = 0;
+        float height = 0;
+
+        for (int i = 0; i < listQuestions.size(); i++) {
+            System.out.println(listQuestions.get(i));
+            switch (i){
+                case 0:
+                    name = sc.nextLine();
+                    break;
+                case 1:
+                    email = sc.nextLine();
+                    break;
+                case 2:
+                    while (!sc.hasNextInt()) {  // Verificar se a entrada é um número inteiro
+                        System.out.println("Por favor, insira uma idade válida:");
+                        sc.next(); 
+                    }
+                    age = sc.nextInt();
+                case 3:
+                    while (!sc.hasNextFloat()) {  // Verificar se a entrada é um número de ponto flutuante
+                        System.out.println("Por favor, insira uma altura válida:");
+                        sc.next();  // Descarta a entrada inválida
+                    }
+                    height = sc.nextFloat();
+                    break;
+            }
+        }
+
+        clients.add(new Client(name,
+                email,
+                age,
+                height));
+
+        for (Client c:
+             clients) {
+            System.out.println(c);
         }
     }
 }
---
+++ src/entities/Client.java
@@ -0,0 +1,52 @@
+package entities;
+
+public class Client {
+    private String name;
+    private String email;
+    private int age;
+    private float height;
+
+    public Client(String name, String email, int age, float height) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + "\n" + getEmail() + "\n" + getAge() + "\n" + getHeight();
+    }
+}
